76fb45be0fe6b5230904ab122e5fd20606a0128e
Denis Gorchakov <de.gorchakov@gmail.com>
2022-10-03 20:51:45 +0800

    modal WIP

--- src/js/app.js
+++ src/js/app.js
@@ -28,8 +28,44 @@ class Slider {
 }
 
 
+//* Modal
+class Modal {
+  constructor() {
+    this.modal = document.querySelector('.modal');
+    this.trigger = document.querySelector('.modal-trigger');
+    this.closeButton = document.querySelector('.close-button');
+  }
+
+  test() {
+    console.log(this.modal);
+    console.log(this.trigger);
+    console.log(this.modal.classList);
+    console.log(this.closeButton);
+  }
+
+  toggleModal() {    
+    this.modal.classList.toggle("show-modal");
+  }
+
+  windowOnClick(e) {
+    if (e.target === this.modal) {
+      this.toggleModal();
+    }
+  }
+
+  modalEvents() {
+    this.trigger.addEventListener("click", this.test);
+    this.closeButton.addEventListener("click", this.toggleModal);
+    window.addEventListener("click", this.windowOnClick);
+  }
+}
+
+
 //* Main
 
+const modal = new Modal();
+modal.modalEvents();
+// modal.test();
 
 try {
   loadSlider();
@@ -87,5 +123,7 @@ function loadHamburger() {
     navbarItems.classList.toggle('is-active');
   })
 }
+
+
 
 
